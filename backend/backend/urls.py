3022ddf27cd235a4126d279b37cb9292e00dbabd
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 22:18:00 +0330

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from core import views as core_views
 
 urlpatterns = [
+    path("", core_views.home, name="home"),
     path("admin/", admin.site.urls),
+    path("core/", include("core.urls")),
     path("blog/", include("blog.urls")),
+    path("social/", include("social.urls")),
 ]
