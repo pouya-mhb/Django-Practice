da802cec92cd6fd845c636635e3a4db6fe1cb36f
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 15:54:36 +0330

    add model managers

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -3,7 +3,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status="published")
+
+
 class Post(models.Model):
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()
     STATUS_CHOICES = (
         ("draft", "Draft"),
         ("published", "Published"),
