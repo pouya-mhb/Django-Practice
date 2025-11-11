8082078fc2fc2636fcd35a8b7896fa3d314a03d9
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 16:09:30 +0330

    add blog post list and post detail views

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = Post.objects.get(
+        slug=post, publish__year=year, publish__month=month, publish__day=day
+    )
+    return render(request, "blog/post/detail.html", {"post": post})
+
+
+def year_archive(request, year):
+    posts = Post.published.filter(publish__year=year)
+    return render(
+        request, "blog/post/year_archive.html", {"posts": posts, "year": year}
+    )
+
+
+def month_archive(request, year, month):
+    posts = Post.published.filter(publish__year=year, publish__month=month)
+    return render(
+        request,
+        "blog/post/month_archive.html",
+        {"posts": posts, "year": year, "month": month},
+    )
