a33cffc6c21937cb0c4b8fabd709dacfd9cfc6a0
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 21:00:29 +0330

    add similar posts feature

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404, render
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -49,6 +50,13 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # list of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "blog/post/detail.html",
@@ -57,6 +65,7 @@ def post_detail(request, year, month, day, post):
             "comments": comments,
             "new_comment": new_comment,
             "comment_form": comment_form,
+            "similar_posts": similar_posts,
         },
     )
 
