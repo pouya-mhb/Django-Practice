f7832a6788dc52f58420349ac8365aa95151779c
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-11 19:54:59 +0330

    Update urls.py

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
     path("<int:year>", views.year_archive, name="year_archive"),
     path("<int:year>/<int:month>", views.month_archive, name="month_archive"),
-    # path(
-    #     "<int:year>/<int:month>/<int:day>/<slug:post>",
-    #     views.post_detail,
-    #     name="post_detail",
-    # ),
     path(
-        "<int:year>/<int:month>/<int:day>/<slug:slug>",
-        views.PostDetail.as_view(),
+        "<int:year>/<int:month>/<int:day>/<slug:post>",
+        views.post_detail,
         name="post_detail",
     ),
+    # path(
+    #     "<int:year>/<int:month>/<int:day>/<slug:slug>",
+    #     views.PostDetail.as_view(),
+    #     name="post_detail",
+    # ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
