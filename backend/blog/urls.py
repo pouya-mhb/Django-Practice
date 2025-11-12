cb251d5c921ec2c97f9e545d9a71d2a9a95e2c5b
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-12 09:18:33 +0330

    add create, dashboard to urls

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     # ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
+    path("create/", views.post_create, name="post_create"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
