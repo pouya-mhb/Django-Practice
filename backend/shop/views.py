70054172279729821af814dee3fc58f5a5c3334b
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-12 16:44:18 +0330

    add shop views

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(
+        request,
+        "shop/product_list.html",
+        {"products": products, "categories": categories},
+    )
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, "shop/product_detail.html", {"product": product})
