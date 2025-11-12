066a20ed1b76921f86b01e517c8a7d06a2d24d4c
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-12 16:43:48 +0330

    register shop admin models

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "slug")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "category", "price", "stock", "created_at")
+    list_filter = ("category",)
