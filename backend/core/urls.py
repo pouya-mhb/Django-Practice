8e22e24e67dbc936112e61cbcb3541f8024bf609
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-12 12:25:14 +0330

    add password reset urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from core import views
-from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import (
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.urls import reverse_lazy
 
 app_name = "core"
@@ -21,5 +28,28 @@ urlpatterns = [
         "password_change/done/",
         PasswordChangeDoneView.as_view(),
         name="password_change_done",
+    ),
+    # reset password urls
+    path(
+        "password_reset/",
+        PasswordResetView.as_view(success_url=reverse_lazy("core:password_reset_done")),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy("core:password_reset_complete")
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
     ),
 ]
