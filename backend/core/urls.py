7d9bea40f56f6a6ca3fb2a89f2d857b61f4beb64
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-11-12 11:38:24 +0330

    add change password url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from core import views
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.urls import reverse_lazy
 
 app_name = "core"
 
@@ -7,4 +9,17 @@ urlpatterns = [
     path("login/", views.user_login, name="login"),
     path("register/", views.user_register, name="register"),
     path("logout/", views.user_logout, name="logout"),
+    # change password urls
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(
+            success_url=reverse_lazy("core:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
